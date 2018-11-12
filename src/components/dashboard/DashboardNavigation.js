ebb519df1bd10cef344f71d9ad3978c4252828b9
justin <justin.martin.developper@gmail.com>
2018-11-12 12:32:32 +0100

    side nav dashboard

--- src/components/dashboard/DashboardNavigation.js
+++ src/components/dashboard/DashboardNavigation.js
@@ -1,154 +1,37 @@
 import {Component} from "react";
 import React from "react";
-import StatsLed from "./utils/StatsLed";
-import StatsButton from "./utils/StatsButton";
-import Circle from 'react-circle';
+import DashboardNavigationItem from "./navigation/DashboardNavigationItem";
 
 class DashboardNavigation extends Component {
     render() {
         return (
             <div className="dashboard-navigation">
                 <ul id="slide-out" className="sidenav sidenav-fixed">
-                    <li><div className="row">
-                        <div className="card">
-                            <div className="card-content">
-                                <span className="card-title">Cours n°1</span>
-                                <div className="row valign-wrapper">
-                                    <div className="col s4">
-                                        <Circle
-                                            animate={true} // Boolean: Animated/Static progress
-                                            animationDuration="1s" //String: Length of animation
-                                            size={75} // Number: Defines the size of the circle.
-                                            progress={35}
-                                        />
-                                    </div>
-                                    <div className="col s2">
-                                        <StatsLed state={true}/>
-                                    </div>
-                                    <div className="col s6  ">
-                                        <StatsButton
-                                            online={true}
-                                            validText="Activer"
-                                            invalidText="Désactiver"
-                                            validIcon="publish"
-                                            invalidIcon="remvoe_circle_outline"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div></li>
-                    <li> <div className="row">
-                        <div className="card">
-                            <div className="card-content">
-                                <span className="card-title">Cours n°2</span>
-                                <div className="row valign-wrapper">
-                                    <div className="col s4">
-                                        <Circle
-                                            animate={true} // Boolean: Animated/Static progress
-                                            animationDuration="1s" //String: Length of animation
-                                            size={75} // Number: Defines the size of the circle.
-                                            progress={12}
-                                        />
-                                    </div>
-                                    <div className="col s2">
-                                        <StatsLed state={true}/>
-                                    </div>
-                                    <div className="col s6  ">
-                                        <StatsButton
-                                            online={true}
-                                            validText="Activer"
-                                            invalidText="Désactiver"
-                                            validIcon="publish"
-                                            invalidIcon="remvoe_circle_outline"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div></li>
-                    <li> <div className="row">
-                        <div className="card">
-                            <div className="card-content">
-                                <span className="card-title">Cours n°2</span>
-                                <div className="row valign-wrapper">
-                                    <div className="col s4">
-                                        <Circle
-                                            animate={true} // Boolean: Animated/Static progress
-                                            animationDuration="1s" //String: Length of animation
-                                            size={75} // Number: Defines the size of the circle.
-                                            progress={12}
-                                        />
-                                    </div>
-                                    <div className="col s2">
-                                        <StatsLed state={true}/>
-                                    </div>
-                                    <div className="col s6  ">
-                                        <StatsButton
-                                            online={true}
-                                            validText="Activer"
-                                            invalidText="Désactiver"
-                                            validIcon="publish"
-                                            invalidIcon="remvoe_circle_outline"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div></li>
-                    <li> <div className="row">
-                        <div className="card">
-                            <div className="card-content">
-                                <span className="card-title">Cours n°2</span>
-                                <div className="row valign-wrapper">
-                                    <div className="col s4">
-                                        <Circle
-                                            animate={true} // Boolean: Animated/Static progress
-                                            animationDuration="1s" //String: Length of animation
-                                            size={75} // Number: Defines the size of the circle.
-                                            progress={12}
-                                        />
-                                    </div>
-                                    <div className="col s2">
-                                        <StatsLed state={true}/>
-                                    </div>
-                                    <div className="col s6  ">
-                                        <StatsButton
-                                            online={true}
-                                            validText="Activer"
-                                            invalidText="Désactiver"
-                                            validIcon="publish"
-                                            invalidIcon="remvoe_circle_outline"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div></li>
-                    <li><div className="row">
-                        <div className="card">
-                            <div className="card-content">
-                                <span className="card-title">Cours n°2</span>
-                                <div className="row valign-wrapper">
-                                    <div className="col s4">
-                                        <Circle
-                                            animate={true} // Boolean: Animated/Static progress
-                                            animationDuration="1s" //String: Length of animation
-                                            size={75} // Number: Defines the size of the circle.
-                                            progress={12}
-                                        />
-                                    </div>
-                                    <div className="col s2">
-                                        <StatsLed state={true}/>
-                                    </div>
-                                    <div className="col s6  ">
-                                        <StatsButton
-                                            online={true}
-                                            validText="Activer"
-                                            invalidText="Désactiver"
-                                            validIcon="publish"
-                                            invalidIcon="remvoe_circle_outline"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div></li>
+                    <DashboardNavigationItem
+                        percent="50"
+                        nomCours="Cours 1"
+                        online="true"
+                    />
+                    <DashboardNavigationItem
+                        percent="64"
+                        nomCours="Cours 2"
+                        online="true"
+                    />
+                    <DashboardNavigationItem
+                        percent="12"
+                        nomCours="Cours 3"
+                        online="false"
+                    />
+                    <DashboardNavigationItem
+                        percent="98"
+                        nomCours="Cours 4"
+                        online="false"
+                    />
+                    <DashboardNavigationItem
+                        percent="35"
+                        nomCours="Cours 5"
+                        online="true"
+                    />
                 </ul>
                 <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
             </div>
